78891e1cd23b49da733fb9e907f14c78c9d1bf52
Feanor <hamzaaygan1995@gmail.com>
2023-04-12 17:00:25 +0300

    OrderItemMVS update method

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -43,10 +43,8 @@ class OrderItemMVS(ModelViewSet):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
-        # obj = get_object_or_404(Item, id=request.data["item_id"])
         orderItem_queryset = OrderItem.objects.filter(user_id=request.user.id, item_id=request.data["item_id"])
         
-
         if orderItem_queryset.exists():
             data ={
                 "message": "You already have this item"
@@ -59,9 +57,24 @@ class OrderItemMVS(ModelViewSet):
             }
             return Response(data, status=status.HTTP_201_CREATED)
 
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
 
-        
+        if 'quantity' in request.data and request.data['quantity'] == 0:
+            self.perform_destroy(instance)
+            return Response(status=status.HTTP_204_NO_CONTENT)
 
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
 
 class OrderMVS(ModelViewSet):
     queryset = Order.objects.all()
